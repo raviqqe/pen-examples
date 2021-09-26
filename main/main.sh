ea35cde033fbb61d4c8b50c54bc56f18ddd1cd5f
Yota Toyama <raviqqe@gmail.com>
2021-09-26 04:08:39 +0000

    Update

--- main/main.sh
+++ main/main.sh
@@ -7,4 +7,5 @@ pen build --target wasm32-wasi
 wasm2wat --enable-tail-call app >app.wat
 node \
   --experimental-wasi-unstable-preview1 \
-  --experimental-wasm-return_call run.js
+  --experimental-wasm-return_call \
+  run.js
